aea2b6f0674af9f3b876cc5565fc286f95b2e063
Dmitry Fedyuk <dfediuk@gmail.com>
2022-02-19 20:40:07 +0600

    https://github.com/dmitry-ai/aws-gpu-2022-02-06/issues/2

--- home/ubuntu/.jupyter/jupyter_notebook_config.py
+++ home/ubuntu/.jupyter/jupyter_notebook_config.py
@@ -5,4 +5,7 @@ c.NotebookApp.iopub_data_rate_limit = 10000000000
 # 2.1) https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#using-let-s-encrypt
 # 2.2) https://github.com/jupyter/notebook/blob/v6.4.8/docs/source/public_server.rst#using-lets-encrypt
 c.NotebookApp.certfile = u'/etc/letsencrypt/live/aws.dmitry.ai/fullchain.pem'
+c.NotebookApp.ip = '*'
 c.NotebookApp.keyfile = u'/etc/letsencrypt/live/aws.dmitry.ai/privkey.pem'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 9999
